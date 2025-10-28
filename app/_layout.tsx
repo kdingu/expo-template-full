cf1b6cea23527275b59f1102b1caa98d9b5b3df7
Klaidi Dingu <klaididingu@gmail.com>
2025-10-28 13:32:09 +0100

    add gesture handler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
-import { Stack } from "expo-router";
-
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import "@/global.css";
 
 export default function RootLayout() {
 	return (
-		<GluestackUIProvider>
-			<Stack screenOptions={{ headerShown: false }}></Stack>
-		</GluestackUIProvider>
+		<GestureHandlerRootView>
+			<GluestackUIProvider>
+				<Stack screenOptions={{ headerShown: false }}></Stack>
+			</GluestackUIProvider>
+		</GestureHandlerRootView>
 	);
 }
