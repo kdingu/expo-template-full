8b73025800d4d1a6cc05aaf0b49f5d8d803cf10f
Klaidi Dingu <klaididingu@gmail.com>
2025-10-28 14:08:07 +0100

    add tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import "@/global.css";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
 	return (
-		<GestureHandlerRootView>
-			<GluestackUIProvider>
-				<Stack screenOptions={{ headerShown: false }}></Stack>
-			</GluestackUIProvider>
-		</GestureHandlerRootView>
+		<QueryClientProvider client={queryClient}>
+			<GestureHandlerRootView>
+				<GluestackUIProvider>
+					<Stack screenOptions={{ headerShown: false }}></Stack>
+				</GluestackUIProvider>
+			</GestureHandlerRootView>
+		</QueryClientProvider>
 	);
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import pluginQuery from "@tanstack/eslint-plugin-query";
+
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+	expoConfig,
+	...pluginQuery.configs["flat/recommended"],
+	{
+		ignores: ["dist/*"],
+	},
 ]);
